14bb5ca874f99bf330e0ad2fd876a28daf125fde
Emo <e.moskov@draftkings.com>
2022-11-04 11:16:04 +0200

    added FluentValidation

---
+++ CarService/Validators/ClientValidator.cs
@@ -0,0 +1,16 @@
+﻿using CarService.Models.Models;
+using FluentValidation;
+
+namespace CarService.Validators
+{
+    public class ClientValidator : AbstractValidator<Client>
+    {
+        public ClientValidator()
+        {
+            RuleFor(x => x.ClientName).NotEmpty().MinimumLength(2).MaximumLength(50);
+            RuleFor(x => x.City).NotEmpty().MinimumLength(2).MaximumLength(50);
+            RuleFor(x => x.Age).NotNull().NotEmpty().GreaterThan(18).LessThan(80);
+            RuleFor(x => x.CarId).NotEmpty().GreaterThan(0);
+        }
+    }
+}
